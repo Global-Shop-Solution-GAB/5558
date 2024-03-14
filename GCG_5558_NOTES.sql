01e53842fe450f1eb5d32aecb1f82c2522d99db6
Service Web <sw@gssmail.com>
2024-03-14 12:23:43 -0500

    Commit changes in C:\Websites\Git\emarlow\5558\GCG_5558_NOTES.sql

---
+++ GCG_5558_NOTES.sql
@@ -0,0 +1,12 @@
+/* ==========================================
+ * TABLE: GCG_5558_NOTES
+ * ========================================== */
+CREATE TABLE "GCG_5558_NOTES"(
+ "SALES_ORDER" CHAR(7),
+ "SALES_ORDER_LINE" CHAR(3),
+ "COMPANY_NOTES" LONGVARCHAR,
+ "COMPANY_UPDATED" DATE,
+ "CUSTOMER_NOTES" LONGVARCHAR,
+ "CUSTOMER_UPDATED" DATE);
+
+
