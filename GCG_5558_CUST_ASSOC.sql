6ca2d8c0abbde1f428a1a17c64ed657824f0c684
Service Web <sw@gssmail.com>
2024-03-14 12:23:43 -0500

    Commit changes in C:\Websites\Git\emarlow\5558\GCG_5558_CUST_ASSOC.sql

---
+++ GCG_5558_CUST_ASSOC.sql
@@ -0,0 +1,9 @@
+/* ==========================================
+ * TABLE: GCG_5558_CUST_ASSOC
+ * ========================================== */
+CREATE TABLE "GCG_5558_CUST_ASSOC"(
+ "CUSTOMER" CHAR(6),
+ "GSS_USER" CHAR(8),
+ "GSS_PASSWORD" CHAR(50));
+
+
